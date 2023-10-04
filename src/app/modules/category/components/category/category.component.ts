ea38118a39c26a63ef482ef12b25ad3f5ff2ad2c
Nicolas Garcia <zextnigarcia@correoargentino.com.ar>
2023-10-04 16:01:40 -0300

    update_paginator

--- src/app/modules/category/components/category/category.component.ts
+++ src/app/modules/category/components/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { MatTab } from '@angular/material/tabs';
 import { CategoryService } from 'src/app/modules/shared/services/category.service';
 
 @Component({
@@ -14,15 +16,46 @@ export class CategoryComponent implements OnInit{
     this.getCategories();
   }
 
+  displayedColumns: string[] = ['id', 'name', 'description', 'actions'];
+  dataSource = new MatTableDataSource<CategoryElement>();
+
+
   getCategories () : void {
     this.categoryService.getCategories()
     .subscribe( (data : any) => {
 
       console.log("respuesta categorias: ", data);
+      this.processCategoriesResponse(data);
 
     }),
     (error : any) => { 
         console.log("error categorias: ", error);
       }
+
   }
+
+  processCategoriesResponse (resp: any){
+
+    const dataCategory: CategoryElement[] = [];
+
+    if (resp.metadata[0].code == "00"){
+
+      let listCategory = resp.categoryResponse.category;
+
+      listCategory.forEach((element: CategoryElement) => {
+        dataCategory.push(element);
+      });
+
+      this.dataSource = new MatTableDataSource<CategoryElement>(dataCategory);
+
+    }
+
+  }
+
+}
+
+export interface CategoryElement {
+  id: number;
+  name: string;
+  description: string;
 }
